aab02ee167b583211000ddb699c534ce054a489c
Henrik Lievonen <henrik.lievonen@hotmail.com>
2015-02-25 19:49:10 +0200

    Keyboard handler. Player can be moved

--- src/entity.ts
+++ src/entity.ts
@@ -1,6 +1,11 @@
 ﻿class Entity {
 
     constructor(public x: number, public y: number, public width: number, public height: number) { }
+
+    move(x: number, y: number) {
+        this.x += x;
+        this.y += y;
+    }
 }
 
 export = Entity;
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,7 @@
 ﻿import Renderer = require('renderer');
 import Player = require('player');
 import Map = require('map');
+import Keyboard = require('keyboard');
 
 class Game {
     private _players: Player[];
@@ -8,17 +9,28 @@ class Game {
     private _runner: () => void;
     private _map: Map;
 
-    constructor(private _renderer: Renderer) {
+    constructor(private _renderer: Renderer, private _keyboard: Keyboard) {
         // TODO: Get map from somewhere else
         this._map = new Map(10, 10);
 
-        this._currentPlayer = new Player(2,2);
+        this._currentPlayer = new Player(2, 2);
         this._players = [];
         this._players.push(this._currentPlayer);
     }
 
     get renderer(): Renderer {
         return this._renderer;
+    }
+
+    update(): void {
+        if (this._keyboard.isKeyDown(Keyboard.Key.Left))
+            this._currentPlayer.move(-1, 0);
+        if (this._keyboard.isKeyDown(Keyboard.Key.Right))
+            this._currentPlayer.move(1, 0);
+        if (this._keyboard.isKeyDown(Keyboard.Key.Up))
+            this._currentPlayer.move(0, -1);
+        if (this._keyboard.isKeyDown(Keyboard.Key.Down))
+            this._currentPlayer.move(0, 1);
     }
 
     render(): void {
@@ -34,15 +46,18 @@ class Game {
     }
 
     start(): void {
+        // Create the runner
         this._runner = this.run.bind(this);
+        // And run the first step which will continue running
         this.run();
     }
 
     private run(): void {
-
+        // Game loop
+        this.update();
         this.render();
         
-        // Run the next step on next update
+        // Continue the game loop
         window.requestAnimationFrame(this._runner);
     }
 }
